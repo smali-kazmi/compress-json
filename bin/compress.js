4c502ef5ca96114d5e2ec19c3971074319f09576
SM@K <smali.kazmi@hotmail.com>
2016-06-21 05:50:23 +0500

    add get Header function

--- bin/compress.js
+++ bin/compress.js
@@ -1,12 +1,44 @@
 function compress(json)
 {
-  var header = [];
-  var data = [];
-
+  
   var final = {
     header: [],
     data: []
   };
 
+
+  var len = json.length;
+
+  if(len > 0) {
+
+    for(var x = 0; x < len; x++) {
+
+      if(final.header.length === 0) {
+        final.header = getHeader(json[x]);
+      }
+
+      final.data.push(getValues(json[x]));
+    }
+
+  }
+
+  return final;
+
 }
 
+function getHeader(obj) 
+{
+    return Object.keys(obj);
+}
+
+
+function getValues(obj) 
+{
+  var val = [];
+
+  for(x in obj) {
+    val.push(obj[x]);
+  }
+
+  return val;
+}
